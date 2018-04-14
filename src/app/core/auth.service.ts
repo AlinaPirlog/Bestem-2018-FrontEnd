30e1f7b7e679a9aabc3494eb8a875b24911c49e5
AI Alexandru Irimia <e2500109@agna.amgreetings.com>
2018-04-15 02:24:07 +0300

    e de la mac

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,19 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { TokenStorage } from "./token.storage";
 @Injectable()
 export class AuthService {
 
-  private baseUrl : string = 'http://192.168.21.134:8091/auth';
+  private baseUrl : string = 'http://192.168.21.134:8091';
+  headers: HttpHeaders;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private tokenStorage: TokenStorage) {
+    this.headers = new HttpHeaders().set('content-type','application/json');
+    this.headers.append('Authorization', 'Bearer ' + this.tokenStorage.getToken());
+
   }
 
-  attemptAuth(ussername: string, password: string): Observable<any> {
-    const credentials = {username: ussername, password: password};
+  attemptAuth(username: string, password: string): Observable<any> {
+    const credentials = {username: username, password: password};
     console.log('attempAuth ::');
-    return this.http.post<any>(this.baseUrl, credentials);
+    return this.http.post<any>(this.baseUrl + "/com/auth", credentials);
+  }
+
+  getItineraries():Observable<any> {
+    return this.http.get<any>(this.baseUrl+"/api/itinerary/get",{headers:this.headers});
+  }
+
+  createNewItinerary(data):Observable<any> {
+    return this.http.post<any>(this.baseUrl+"/api/itinerary/create",data,{headers:this.headers});
   }
 
 }
