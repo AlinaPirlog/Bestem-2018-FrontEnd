4b5142cdb8a30a59c8b78ff5c5b9c4c77915b40f
AI Alexandru Irimia <e2500109@agna.amgreetings.com>
2018-04-14 16:08:09 +0300

    remove from service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
-import {User} from './user/user.model';
+
 
 
 const httpOptions = {
@@ -12,11 +12,4 @@ const httpOptions = {
 export class UserService {
 
   constructor(private http: HttpClient) {}
-
-  private userUrl = 'http://localhost:8080/';
-
-  public getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.userUrl + '/users');
-  }
-
 }
